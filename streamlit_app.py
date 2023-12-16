1d84d1ad21a3ba80a785d216b4e77039888820e1
robert-marik <robert.marik.cz@gmail.com>
2023-12-17 00:19:33 +0100

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,36 +10,68 @@ r"""
 Rovnice je logistická rovnice obohacená o konstantní lov. Používá se k modelování ekologicky udržitelného 
 lovu v populaci žijící v prostředí s omezenou nosnou kapacitou.
 
-$$ \displaystyle \frac{\mathrm dx}{\mathrm dt}=rx\left(1-\frac xK\right)-h$$
+$$ 
+\frac{\mathrm dx}{\mathrm dt}=rx\left(1-\frac xK\right)-h
+$$
 
 """
 
 col1, col2 = st.columns(2)
 
 with col1:
-    K = st.slider("K", 0.1, 10.0, 1.0)
-    r = st.slider("r", .1, 10.0, 1.0)
-    h = st.slider("h", 0.0, 1.0, .1)
+    K = st.slider(r"$K$ (nosná kapacita prostředí)", 0.1, 10.0, 1.0)
+    r = st.slider(r"$r$ (rychlost růstu)", .1, 10.0, 1.0)
+    h = st.slider(r"$h$ (itenzita lovu)", 0.0, 1.0, .1)
 
 meze = [0,10]
 n = 1000
 t = np.linspace(*meze,n)
 N = 50
-reseni = np.zeros((n,N))
+reseni = np.empty((n,N))
+reseni[:,:] = np.nan
+
+def destrukce_populace(t,x):  # Pokud x klesne na nulu, zastavíme výpočet
+    return x
+destrukce_populace.terminal = True
 
 # %%
 for i,pp in enumerate(np.linspace(0.05,K*1.4,N)):
     sol = solve_ivp(
-                   lambda t,x:0 if x<0 else r*x*(1-x/K)-h,
+                   lambda t,x: r*x*(1-x/K)-h,
                    meze,
                    [pp],
-                   t_eval=t
+                   t_eval=t,
+                   events=destrukce_populace,
                    ).y
     reseni[:sol.size,i] = sol
 
+# %%
+
 fig, ax = plt.subplots()    
-ax.plot(t,reseni, color='gray')
-ax.set(ylim=(0,reseni.max()))
+
+for res in reseni.T:
+    # if np.isnan(res).any():
+    if (r*K/2*(1-K/2/K)<h) or (res[0]< K/2 and r*res[0]*(1-res[0]/K)-h)<0:
+        color='red'
+    else:
+        color='gray'
+    ax.plot(t,res,color=color)
+
+ax.set(ylim=(0,np.nanmax(reseni)))
+
+
+# %%
+fig2,ax2 = plt.subplots()
+x = np.linspace(0,1.3*K,500)
+ax2.plot(x,r*x*(1-x/K), color='blue')
+ax2.plot(x,h+x*0, color='red')
+ax2.set(ylim=(0,None))
+
+
 
 with col2:
-    st.pyplot(fig)
+    tab1,tab2 = st.tabs(["Časový vývoj","Graf pravé strany"])
+    with tab1:
+        st.pyplot(fig)
+    with tab2:
+        st.pyplot(fig2)
