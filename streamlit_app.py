b6f6fbd44b456424108b6b44c4cdd9d756915c4a
robert-marik <robert.marik.cz@gmail.com>
2023-12-16 22:17:24 +0100

    ODE

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,36 @@
 import altair as alt
 import numpy as np
-import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+
+r"""
+# Logistická rovnice s lovem
+
+$$ \displaystyle \frac{\mathrm dx}{\mathrm dt}=rx\left(1-\frac xK\right)-h$$
 
 """
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+K = st.slider("K", 0.1, 10.0, 1.0)
+r = st.slider("r", .1, 10.0, 1.0)
+h = st.slider("h", 0.0, 1.0, .1)
+meze = [0,10]
+n = 1000
+t = np.linspace(*meze,n)
+N = 50
+reseni = np.zeros((n,N))
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# %%
+for i,pp in enumerate(np.linspace(0.05,K*1.4,N)):
+    sol = solve_ivp(
+                   lambda t,x:0 if x<0 else r*x*(1-x/K)-h,
+                   meze,
+                   [pp],
+                   t_eval=t
+                   ).y
+    reseni[:sol.size,i] = sol
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+fig, ax = plt.subplots()    
+ax.plot(t,reseni, color='gray')
+ax.set(ylim=(0,reseni.max()))
+st.pyplot(fig)
